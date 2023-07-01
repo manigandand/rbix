6ec36c8727559e3054e70871687588f3f4391874
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2023-07-01 17:44:37 +0530

    basic box simulations

---
+++ rbi/README.md
@@ -0,0 +1,11 @@
+### RBI - Remote Browser Isolation
+
+- Pixel pushing/streaming
+- Page scrubbing
+
+```
+CGO_ENABLED=0 GOOS=linux go build -o sqrx-rbi -ldflags "-w -s"
+
+```
+
+# TODO: build scripts
---
+++ rbi/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/shirou/gopsutil/v3/host"
+)
+
+func main() {
+	port := ":8888"
+	http.HandleFunc("/", handler)
+	log.Println("rbi listening on ", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	hostInfo, err := host.Info()
+	if err != nil {
+		log.Println("[host.Info() Error] %v", err.Error())
+	}
+	log.Println("host info:", hostInfo)
+
+	res := map[string]interface{}{
+		"host":      r.Host,
+		"host_info": hostInfo,
+		"data": map[string]interface{}{
+			"hostname":     "12",
+			"container_id": "123",
+			"message":      "you're in safe zone with rbi",
+		},
+		"meta": map[string]interface{}{
+			"status":      "ok",
+			"status_code": http.StatusOK,
+		},
+	}
+
+	resp, err := json.Marshal(&res)
+	if err != nil {
+		log.Println("error:", err.Error())
+		return
+	}
+
+	w.Header().Set("Host", r.Host)
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := w.Write(resp); err != nil {
+		log.Fatal(err.Error())
+	}
+}
