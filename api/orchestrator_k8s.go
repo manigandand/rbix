502b30109b36f5f1636f4085479bad52fec5366e
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2023-07-05 12:52:24 +0530

    add empty k8s orchestrator implementations

--- api/orchestrator_k8s.go
+++ api/orchestrator_k8s.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"context"
+
+	"github.com/manigandand/adk/errors"
+)
+
+type k8s struct {
+}
+
+// NewK8sOrchestrator - create new k8s orchestrator
+func NewK8sOrchestrator() (Orchestrator, *errors.AppError) {
+	return &k8s{}, nil
+}
+
+// StartRBIInstance - start new sqrx-rbi instance
+func (k *k8s) StartRBIInstance(ctx context.Context, containerUniqeID string,
+) (*ContainerInfo, *errors.AppError) {
+	return nil, nil
+}
+
+// DestroyRBIInstance - destroy sqrx-rbi instance
+func (k *k8s) DestroyRBIInstance(ctx context.Context, terminationToken string) *errors.AppError {
+	return nil
+}
+
+// Stop - stop orchestrator
+func (k *k8s) Stop() {}
