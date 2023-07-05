8e1d815b4eb46a215a4748ddee92a7d6cd78d60f
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2023-07-05 12:51:54 +0530

    add orchestrator interface

---
+++ api/orchestrator.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/manigandand/adk/errors"
+)
+
+// orchestrator - global orchestrator instance
+var orchestrator Orchestrator
+
+// Orchestrator - orchestrator interface
+type Orchestrator interface {
+	// StartRBIInstance - start new sqrx-rbi instance
+	StartRBIInstance(ctx context.Context, containerUniqeID string) (*ContainerInfo, *errors.AppError)
+
+	// DestroyRBIInstance - destroy sqrx-rbi instance
+	DestroyRBIInstance(ctx context.Context, terminationToken string) *errors.AppError
+
+	// Stop - stop orchestrator
+	Stop()
+}
+
+// InitOrchechestrator - init orchestrator
+func InitOrchechestrator() {
+	// init docker orchestrator
+	if Env == EnvLoclDocker {
+		docOrch, err := NewDockerOrchestrator()
+		if err.NotNil() {
+			log.Fatal("could not create docker orchestrator: " + err.Error())
+		}
+
+		orchestrator = docOrch
+		log.Println("docker orchestrator initialized 👍")
+		return
+	}
+
+	if Env == EnvLocalK8s {
+		k8sOrch, err := NewK8sOrchestrator()
+		if err.NotNil() {
+			log.Fatal("could not create k8s orchestrator: " + err.Error())
+		}
+
+		orchestrator = k8sOrch
+		log.Println("k8s orchestrator initialized 👍")
+		return
+	}
+
+	log.Fatal("unsupported env ", Env)
+}
